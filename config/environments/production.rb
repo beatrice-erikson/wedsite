96a8eb4d281bd284b88d2493251dd9cb34eefab5
Beatrice E. Corfman <bec12@hampshire.edu>
2017-09-09 14:10:30 -0700

    heroku debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
    :port                  => 587,
    :user_name             => ENV['SENDGRID_USERNAME'],
    :password              => ENV['SENDGRID_PASSWORD'],
-   :domain                => "www.beatrice-and-justice.com",
-   :authentication        => "plain",
+   :domain                => "beatrice-and-justice.com",
+   :authentication        => "login",
   :enable_starttls_auto   => true
   }
-  config.action_mailer.default_url_options = { :host => 'www.beatrice-and-justice.com' }
+  config.action_mailer.default_url_options = { :host => 'beatrice-and-justice.com' }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
