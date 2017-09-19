eec71636adbf1198b901f8bfe52e82d789703e95
notthatstraight <bec12@hampshire.edu>
2017-09-19 12:27:12 -0700

    cast db int explicitly for pg

--- db/migrate/20170917215927_change_attending_in_rsvps.rb
+++ db/migrate/20170917215927_change_attending_in_rsvps.rb
@@ -1,11 +1,11 @@
 class ChangeAttendingInRsvps < ActiveRecord::Migration[5.0]
   def self.up
-    change_column :rsvps, :is_attending, :integer
+    change_column :rsvps, :is_attending, 'integer USING CAST(is_attending AS integer)'
     rename_column :rsvps, :is_attending, :response
   end
  
   def self.down
     rename_column :rsvps, :response, :is_attending
-    change_column :rsvps, :is_attending, :boolean
+    change_column :rsvps, :is_attending, 'boolean USING CAST(is_attending AS boolean)'
   end
 end
