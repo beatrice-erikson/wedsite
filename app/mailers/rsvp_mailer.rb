a83be8e50d987337c816c351f26be26b309188e4
notthatstraight <bec12@hampshire.edu>
2018-06-09 19:46:27 -0700

    change email to reminder

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -5,6 +5,6 @@ class RsvpMailer < ActionMailer::Base
     def invite_email(id)
         @person = Rsvp.find(id)
         @rsvp_url = rsvp_link_url(@person.name)
-        mail(to: @person.email, subject: "RSVP for Justice + Bea's wedding!")
+        mail(to: @person.email, subject: "Reminder to RSVP for Justice + Bea's wedding")
     end
 end
